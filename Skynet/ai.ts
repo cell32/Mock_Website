14fac4a0e5905e318a5daeda2028573abe0ed5c1
cell32 <33383872+cell32@users.noreply.github.com>
2020-02-24 11:42:24 -0600

    Add files via upload

---
+++ Skynet/ai.ts
@@ -0,0 +1,33 @@
+
+    
+    var a, b, c;
+    var outputText;
+    
+    function quadraticEq() {
+    
+        // get the input
+        b = document.forms["input_form"]["var_b"].value;
+        a = document.forms["input_form"]["var_a"].value;
+        c = document.forms["input_form"]["var_c"].value;
+        
+        // validate a, b and c
+        if (a == 0) {
+            outputText = "<em>a</em> cannot equal zero!";
+        } else if (isNaN(a)) {
+            outputText = "<em>a</em> must be a number!";
+        } else if (isNaN(b)) {
+            outputText = "<em>b</em> must be a number!";
+        } else if (isNaN(c)) {
+            outputText = "<em>c</em> must be a number!";
+        } else {
+            // calculate the result using x = (-b +- sqrt(b^2 - 4ac)) / 2a
+            var x1 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+            var x2 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+            outputText = "For the equation <strong>" + (a == 1 ? "" : a) + "x\u00B2 + " + (b == 1 ? "" : b) + "x + " + c + " = 0</strong>, x can equal <strong>" + x1 + "</strong> or <strong>" + x2 + "</strong>";
+        }
+        
+        // output the result (or errors)
+        document.getElementById("output_text").innerHTML = outputText;
+    }
+
+
---
+++ Skynet/contactus.js
@@ -0,0 +1,8 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function robot() {
+    document.getElementById("robotList").innerHTML = robot.R_choice;
+
+}
+
+
